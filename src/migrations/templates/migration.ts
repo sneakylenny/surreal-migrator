99672383f71f28417c6c11ede7c674557e744072
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2026-07-17 09:11:26 +0200

    refactor: update migration query comments to use SurQL syntax
    - Modified comments in the migration template to use the SurQL syntax for clarity.
    - Ensured consistency in the comment format for future migration queries.

--- src/migrations/templates/migration.ts
+++ src/migrations/templates/migration.ts
@@ -1,9 +1,9 @@
 import type { Surreal } from "surrealdb";
 
 export async function up(db: Surreal): Promise<void> {
-  // await db.query(`...`);
+  // await db.query(/* surql */ `...`)
 }
 
 export async function down(db: Surreal): Promise<void> {
-  // await db.query(`...`);
+  // await db.query(/* surql */ `...`)
 }
